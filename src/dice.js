03934d4ba3ea80b2f90c5cd3d0f0c289298b6c89
golbian <florian13620@hotmail.fr>
2020-12-17 12:58:18 +0100

    class dice

---
+++ src/dice.js
@@ -0,0 +1,21 @@
+ class Dice {
+    1= {
+        faveur: Boolean
+    };
+    2= {
+        faveur: Boolean
+    };
+
+    3= {
+        faveur: Boolean
+    };
+    4= {
+        faveur: Boolean
+    };
+    5= {
+        faveur: Boolean
+    };
+    6= {
+        faveur: Boolean
+    };
+}
